69b0764a3d1a916ada8b0281ef9913168b222dda
IsranjanSathiyaseelan <isranjansathiyaseelan@gmail.com>
2026-07-30 11:00:14 +0530

    Add database connection setup

---
+++ dental-clinic-management-system/src/db/TestConnection.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package db;
+
+/**
+ *
+ * @author Isranjan
+ */
+public class TestConnection {
+        public static void main(String [] args){
+        DBconnection.getConnection();
+    }
+}
